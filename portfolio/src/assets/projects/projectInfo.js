cef591acb392030498740ec571b1c20ff459ec81
Luis Hurtado <lusiferhc352@gmail.com>
2025-04-29 23:55:02 -0500

    add projects list data

---
+++ portfolio/src/assets/projects/projectInfo.js
@@ -0,0 +1,35 @@
+export const projects = [
+    {
+      id: "1",
+      title: "Pale Blue Dot",
+      image:
+        "https://d33wubrfki0l68.cloudfront.net/60e651d33d335a0007692228/screenshot_2021-07-08-01-16-08-0000.png",
+      date: "03/13/2021",
+      tools: ["HTML", "CSS"],
+      description:
+        "You can find a few podcast regarding technology and programming",
+      link: "https://podcastchannel1.netlify.app/",
+    },
+    {
+      id: "2",
+      title: "Hotel Reservation",
+      image:
+        "https://d33wubrfki0l68.cloudfront.net/60e652b751ff1e0007f85359/screenshot_2021-07-08-01-21-10-0000.png",
+      date: "06/13/2021",
+      tools: ["HTML", "CSS", "JS"],
+      description:
+        "Wherever you want to be, look at the hotels we have around the world and book one",
+      link: "https://hotel-vacation.netlify.app",
+    },
+    {
+      id: "3",
+      title: "Gifos",
+      image:
+        "https://d33wubrfki0l68.cloudfront.net/612127203aa06f2b77ae35bf/screenshot_2021-08-21-16-19-01-0000.png",
+      date: "08/29/2021",
+      tools: ["HTML", "CSS", "ReactJS"],
+      description:
+        "Trending? Spiderman? Cats? All the gifs you want can be found here",
+      link: "https://gifosacamica.netlify.app/",
+    },
+  ];
